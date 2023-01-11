e1b2001a4b263f7bb5c3c287ee0c9ff3bb7b1af6
vincentbachelet-collab <vcbachelet@gmail.com>
2023-01-11 17:01:12 +0100

    version avec Louis

--- src/Components/UploadAvatar/Avatar.tsx
+++ src/Components/UploadAvatar/Avatar.tsx
@@ -6,6 +6,8 @@ const UserAvatarIcon = styled.img`
 	height: ${props => props.height || "80px"};
 	left: 20px;
 	top: 20px;
+	box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+
 	/* Sidebar Hover Text */
 
 	border-radius: 200px;
