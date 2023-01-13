efbef56e1a78e3d6dc0bf49088fcab257e7996ac
louisnfr <louis.raffin@gmail.com>
2023-01-13 14:40:03 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -37,13 +37,13 @@ function App() {
 								path="/"
 								element={<button onClick={toggleTheme}>Toggle Theme</button>}
 							/>
-							{/* <Route path="/" element={<LandingPage />} /> */}
+							<Route path="/" element={<LandingPage />} />
 
-							{/* <Route path="/registration-page" element={<RegistrationPage />} />
+							<Route path="/registration-page" element={<RegistrationPage />} />
 							<Route path="/login-page" element={<LoginPage />} />
-							<Route path="/testImage"/>
+							<Route path="/testImage" />
 							<Route path="/leaderboard" element={<Leaderboard />} />
-							<Route path='/dashboard/:id' element={<Dashboard />} /> */}
+							<Route path="/dashboard/:id" element={<Dashboard />} />
 							<Route path="*" element={<NotFound />} />
 						</Routes>
 					</Router>
