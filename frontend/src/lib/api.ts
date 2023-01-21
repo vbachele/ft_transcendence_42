c39365128d74bde05f3fba7ca786c0ba78c1294e
rcollas <collas.robin@gmail.com>
2023-01-21 18:20:37 +0100

    fix: changed base url to reflect proxy addition

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,27 +1,36 @@
-
-const requestConfig:RequestInit={
-	headers:{
+const requestConfig: RequestInit = {
+	headers: {
 		'Content-Type': 'application/json',
-		'Accept': 'application/json, text/plain',
-	}
-}
+		Accept: 'application/json, text/plain',
+	},
+};
 
-export const api={
-	async get(url:string){
-		return await fetch(import.meta.env.VITE_BACKEND_HOST + url, {...requestConfig, method:'GET'}) // veut dire qu'on spread tous les elements de request config
+export const api = {
+	async get(url: string) {
+		return await fetch('/api' + url, {...requestConfig, method: 'GET'});
 	},
-	async post(url:string, data:unknown)
-	{
-		console.log(import.meta.env.VITE_BACKEND_HOST);
-		return await fetch(import.meta.env.VITE_BACKEND_HOST + url, {...requestConfig, method:'POST', body:JSON.stringify(data) || "{}" }) // si la JSON.stringify(data) est vide on met un objet vide
+	async post(url: string, data: unknown) {
+		return await fetch('/api' + url, {
+			...requestConfig,
+			method: 'POST',
+			body: JSON.stringify(data) || '{}',
+		});
 	},
-	async put(url:string, data:unknown){
-		return await fetch(import.meta.env.VITE_BACKEND_HOST + url, {...requestConfig, method:'PUT', body:JSON.stringify(data) || "{}" }) // si la JSON.stringify(data) est vide on met un objet vide
+	async put(url: string, data: unknown) {
+		return await fetch('/api' + url, {
+			...requestConfig,
+			method: 'PUT',
+			body: JSON.stringify(data) || '{}',
+		});
 	},
-	async patch(url:string, data:unknown){
-		return await fetch(import.meta.env.VITE_BACKEND_HOST + url, {...requestConfig, method:'PATCH', body:JSON.stringify(data) || "{}" }) // si la JSON.stringify(data) est vide on met un objet vide
+	async patch(url: string, data: unknown) {
+		return await fetch('/api' + url, {
+			...requestConfig,
+			method: 'PATCH',
+			body: JSON.stringify(data) || '{}',
+		});
 	},
-	async delete(url:string){
-		return await fetch(import.meta.env.VITE_BACKEND_HOST + url, {...requestConfig, method:'DELETE'}) // veut dire qu'on spread tous les elements de request config
+	async delete(url: string) {
+		return await fetch('/api' + url, {...requestConfig, method: 'DELETE'});
 	},
-}
+};
