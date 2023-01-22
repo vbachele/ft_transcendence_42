9d3bbf6ed110f9a3f6057360aeffd297c399c604
rcollas <collas.robin@gmail.com>
2023-01-22 19:29:54 +0100

    add: env variable support, ports are now fetch from env files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,23 @@
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
+import {defineConfig, loadEnv} from 'vite';
 
-export default {
-	plugins: [react(), svgr(), tsconfigPaths()],
-	server: {
-		proxy: {
-			'/api': {
-				target: 'http://backend:3000',
-				changeOrigin: true,
-				secure: true,
-				rewrite: (path) => path.replace(/^\/api/, ''),
+export default ({mode}) => {
+	const env = loadEnv(mode, process.cwd());
+
+	return defineConfig({
+		plugins: [react(), svgr(), tsconfigPaths()],
+		server: {
+			proxy: {
+				'/api': {
+					target: 'http://backend:' + env.VITE_SERVER_PORT.toString(),
+					changeOrigin: true,
+					secure: true,
+					rewrite: (path) => path.replace(/^\/api/, ''),
+				},
 			},
+			port: parseInt(env.VITE_PORT),
 		},
-	},
+	});
 };
